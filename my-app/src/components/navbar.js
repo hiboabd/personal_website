bad36fa54b3f7da7d18d9e71bca4c1e494423f52
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-10 20:53:27 +0100

    Removed line under links

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -10,10 +10,10 @@ export default class Navbar extends Component {
       <nav>
         <div className="navbar">
         <ul>
-          <li className="navbar-item"><Link to="/" className="navbar-brand"><h3>Home</h3></Link></li>
-          <li className="navbar-item"><Link to="/about" className="nav-link"><h3>About Me</h3></Link></li>
-          <li className="navbar-item"><Link to="/portfolio" className="nav-link"><h3>Portfolio</h3></Link></li>
-          <li className="navbar-item"><Link to="/contact" className="nav-link"><h3>Contact</h3></Link></li>
+          <li className="navbar-item"><Link style={{ textDecoration: 'none' }} to="/" className="navbar-brand"><h3>Home</h3></Link></li>
+          <li className="navbar-item"><Link style={{ textDecoration: 'none' }} to="/about" className="nav-link"><h3>About Me</h3></Link></li>
+          <li className="navbar-item"><Link style={{ textDecoration: 'none' }} to="/portfolio" className="nav-link"><h3>Portfolio</h3></Link></li>
+          <li className="navbar-item"><Link style={{ textDecoration: 'none' }} to="/contact" className="nav-link"><h3>Contact</h3></Link></li>
         </ul>
         </div>
     </nav>
