14383723d70fa7c6501d506007cbfadaa34a5d40
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-10 10:41:51 +0100

    Applied homepage styling
    Added name, containers and colours

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -4,7 +4,12 @@ import React from 'react';
 function Home() {
 
   return (
-    <div> Hello! </div>
+    <div className="wrapper">
+      <div className="bar"></div>
+      <div className="name-container">
+        <h1> Hibo Abdilaahi </h1>
+      </div>
+    </div>
   );
 }
 
