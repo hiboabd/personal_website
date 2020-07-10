fad97c62e595764c41d992bf4950e04f29037624
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-10 15:37:40 +0100

    Added image to page and styling

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,14 +1,19 @@
 import React from 'react';
-
+import image from "../Hibo_Abdilaahi.jpeg"
 
 function Home() {
 
   return (
-    <div className="home-wrapper">
-        <h1> Hi, I'm Hibo Abdilaahi. </h1>
-        <div className="dynamic-text">
-          <p> Junior software developer.</p>
-        </div>
+    <div className="home-container">
+      <div className="home-image">
+        <img src={image} alt="Hibo Abdilaahi"/>
+      </div>
+      <div className="home-text">
+          <h1> Hi, I'm Hibo Abdilaahi. </h1>
+          <div className="dynamic-text">
+            <p> Junior software developer.</p>
+          </div>
+      </div>
     </div>
   );
 }
