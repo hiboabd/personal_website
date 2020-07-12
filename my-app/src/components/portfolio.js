224cdab4700a020ca6f06caf53f6ac10d58660d0
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-12 17:48:22 +0100

    Added sentences to portfolio section

--- my-app/src/components/portfolio.js
+++ my-app/src/components/portfolio.js
@@ -4,7 +4,15 @@ import React from 'react';
 function Portfolio() {
 
   return (
-    <div> <h1> Portfolio </h1> </div>
+    <div className="portfolio">
+      <div className="portfolio-text">
+        <h3>  These are some of my favorite projects. </h3>
+        <h3>  Check out my Github profile to see more. </h3>
+      </div>
+      <div className="portfolio-items">
+
+      </div>
+    </div>
   );
 }
 
