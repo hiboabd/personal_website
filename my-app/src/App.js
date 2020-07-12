77f5da5f5a2a7c8e5224fc1a9935319741916bdb
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-12 17:49:09 +0100

    Add component transitions with react spring

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {useTransition, animated } from 'react-spring';
+import {Switch, Route, useLocation} from 'react-router-dom';
 import './App.css';
 import Home from "./components/home";
 import Navbar from "./components/navbar";
@@ -10,16 +11,29 @@ import Contact from "./components/contact";
 
 
 function App() {
+  const location = useLocation();
+
+  const transitions = useTransition(location, location => location.pathname, {
+    from: {opacity: 0, transform: "translate(100%, 0)"},
+    enter: {opacity: 1, transform: "translate(0%, 0)"},
+    leave: {opacity: 0, transform: "translate(-50%, 0)"}
+  });
 
   return (
-    <Router>
+    <>
       <Navbar/>
-      <Route path="/" exact component={Home}/>
-      <Route path="/about" exact component={About}/>
-      <Route path="/portfolio" exact component={Portfolio}/>
-      <Route path="/contact" exact component={Contact}/>
+        {transitions.map(({ item, props, key}) => (
+          <animated.div key={key} style={props}>
+            <Switch location={item}>
+              <Route path="/" exact component={Home}/>
+              <Route path="/about" exact component={About}/>
+              <Route path="/portfolio" exact component={Portfolio}/>
+              <Route path="/contact" exact component={Contact}/>
+            </Switch>
+          </animated.div>
+        ))}
       <Footer/>
-    </Router>
+    </>
   );
 }
 
