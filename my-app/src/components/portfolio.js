82a511c2147285d0b06232388f4e386ef2112422
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-14 10:59:37 +0100

    Added project titles and descriptions

--- my-app/src/components/portfolio.js
+++ my-app/src/components/portfolio.js
@@ -16,31 +16,30 @@ function Portfolio() {
         <Card bg='info'>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>This website!</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
+              Designed primarily using React and React-Boostrap.
             </Card.Text>
           </Card.Body>
         </Card>
         <Card bg='info'>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Run, Boris Run!</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
+              A 2D platformer game written purely in Vanilla JavaScript, HTML5
+              and CSS. Help Boris Johnson socially distance from passerbys and
+              keep his infection rate under 1.
             </Card.Text>
           </Card.Body>
         </Card>
         <Card bg='info'>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>MakersBnb</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This card has even longer content than the first to
-              show that equal height action.
+              An AirBnB clone built as part of the Makers Academy week 6 group
+              project, written in Ruby and utilising PostgreSQL.
             </Card.Text>
           </Card.Body>
         </Card>
@@ -50,31 +49,31 @@ function Portfolio() {
         <Card bg='info'>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Daily Guardian</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
+              A frontend, single page app that summarises the top 10 Guardian
+              articles daily for your convenience. Written in JavaScript and
+              tested with my own testing framework.
             </Card.Text>
           </Card.Body>
         </Card>
         <Card bg='info'>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Bank Tech Test</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
+              An example of my process to approaching a technical problem,
+              utilising TDD and good OO design skills.
             </Card.Text>
           </Card.Body>
         </Card>
         <Card bg='info'>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Acebook</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This card has even longer content than the first to
-              show that equal height action.
+              A social media platform, similar to Facebook, built as part of the
+              week 8-9 engineering project. Built using a MERN stack.
             </Card.Text>
           </Card.Body>
         </Card>
