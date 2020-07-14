1f439d9a721672b1763cbc9a286bcf1efa894851
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-14 08:53:01 +0100

    Added bootstrap cards to portfolio

--- my-app/src/components/portfolio.js
+++ my-app/src/components/portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import image from "../images/Hibo_Abdilaahi.jpeg"
+import CardGroup from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card'
 
 function Portfolio() {
 
@@ -9,38 +11,43 @@ function Portfolio() {
         <h3>  These are some of my favorite projects. </h3>
         <h3>  Check out my Github profile to see more. </h3>
       </div>
-      <div className="portfolio-card">
 
-      <div class="row">
-        <div class="column">
-          <div class="content">
-            <h3>My Work</h3>
-            <p>Lorem ipsum..</p>
-          </div>
-        </div>
+      <>
+      <CardGroup>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to
+              additional content. This card has even longer content than the first to
+              show that equal height action.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </CardGroup>
+      </>
 
-        <div class="column">
-          <div class="content">
-            <h3>My Work</h3>
-            <p>Lorem ipsum..</p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="content">
-            <h3>My Work</h3>
-            <p>Lorem ipsum..</p>
-          </div>
-        </div>
-
-        <div class="column">
-          <div class="content">
-            <h3>My Work</h3>
-            <p>Lorem ipsum..</p>
-          </div>
-        </div>
-        </div>
-      </div>
     </div>
   );
 }
