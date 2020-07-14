612b3a0ac4f21b5a1bc03021720b4fee9a23c700
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-14 12:57:03 +0100

    Added images to cards and stylesd

--- my-app/src/components/portfolio.js
+++ my-app/src/components/portfolio.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import image from "../images/Hibo_Abdilaahi.jpeg"
+import image from "../images/Homepage_mockup.png"
+import makersbnb from "../images/MakersBnb.png"
+import runboris from "../images/Run_Boris_Run.png"
+import dailyguardian from "../images/Daily_Guardian.png"
+import banktechtest from "../images/Bank_Tech_Test.png"
+import acebook from "../images/Acebook.png"
 import CardDeck from 'react-bootstrap/CardDeck'
 import Card from 'react-bootstrap/Card'
 
@@ -14,18 +19,18 @@ function Portfolio() {
       <>
       <CardDeck style={{display: 'flex', flexDirection: 'row'}}>
         <Card bg='info'>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Header>This website!</Card.Header>
+          <Card.Img variant="top" src={image} />
           <Card.Body>
-            <Card.Title>This website!</Card.Title>
             <Card.Text>
               Designed primarily using React and React-Boostrap.
             </Card.Text>
           </Card.Body>
         </Card>
         <Card bg='info'>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Header>Run, Boris Run!</Card.Header>
+          <Card.Img variant="top" src={runboris} />
           <Card.Body>
-            <Card.Title>Run, Boris Run!</Card.Title>
             <Card.Text>
               A 2D platformer game written purely in Vanilla JavaScript, HTML5
               and CSS. Help Boris Johnson socially distance from passerbys and
@@ -34,9 +39,9 @@ function Portfolio() {
           </Card.Body>
         </Card>
         <Card bg='info'>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Header>MakersBnb</Card.Header>
+          <Card.Img variant="top" src={makersbnb} />
           <Card.Body>
-            <Card.Title>MakersBnb</Card.Title>
             <Card.Text>
               An AirBnB clone built as part of the Makers Academy week 6 group
               project, written in Ruby and utilising PostgreSQL.
@@ -47,9 +52,9 @@ function Portfolio() {
       <br/>
       <CardDeck style={{display: 'flex', flexDirection: 'row'}}>
         <Card bg='info'>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Header>Daily Guardian</Card.Header>
+          <Card.Img variant="top" src={dailyguardian} />
           <Card.Body>
-            <Card.Title>Daily Guardian</Card.Title>
             <Card.Text>
               A frontend, single page app that summarises the top 10 Guardian
               articles daily for your convenience. Written in JavaScript and
@@ -58,9 +63,9 @@ function Portfolio() {
           </Card.Body>
         </Card>
         <Card bg='info'>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Header>Bank Tech Test</Card.Header>
+          <Card.Img variant="top" src={banktechtest} />
           <Card.Body>
-            <Card.Title>Bank Tech Test</Card.Title>
             <Card.Text>
               An example of my process to approaching a technical problem,
               utilising TDD and good OO design skills.
@@ -68,9 +73,9 @@ function Portfolio() {
           </Card.Body>
         </Card>
         <Card bg='info'>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Header>Acebook</Card.Header>
+          <Card.Img variant="top" src={acebook} />
           <Card.Body>
-            <Card.Title>Acebook</Card.Title>
             <Card.Text>
               A social media platform, similar to Facebook, built as part of the
               week 8-9 engineering project. Built using a MERN stack.
