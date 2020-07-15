f4b7b96cd1a25bc9b6eaff565d317af1ea4db2c1
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-15 09:03:58 +0100

    Changed contact to class component

--- my-app/src/components/contact.js
+++ my-app/src/components/contact.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
 
-function Contact() {
+class Contact extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      disabled: false,
+      emailSent: null,
+    }
+  }
+
+render () {
   return (
-    <div className="contact">
-      <h1> Contact </h1>
-    </div>
+    <>
+      <div className="contact-text">
+        <h3> Want to get in touch? </h3>
+        <h3> Send me a message! </h3>
+      </div>
+      <div className="contact-form">
+
+      </div>
+      <div className="contact-text">
+        <h3> You can also find me here </h3>
+      </div>
+      <div className="contact-socials">
+
+      </div>
+    </>
   );
+}
+
 }
 
 export default Contact;
