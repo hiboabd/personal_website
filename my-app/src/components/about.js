ce6d4925aa4bc0950de0127892518b516f679d9a
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-15 12:12:35 +0100

    Added image to about page

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import hibo from '../images/Hibo_Abdilaahi.jpeg'
 
 function About() {
   return (
     <div className="about">
       <h1> About </h1>
+      <img src={hibo} alt="Hibo Abdilaahi" />
     </div>
   )
 }
