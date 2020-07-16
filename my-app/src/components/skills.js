90bef8eefc971ac4baab921edee3f615f3944e97
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-16 19:54:25 +0100

    Added correct images for each container

--- my-app/src/components/skills.js
+++ my-app/src/components/skills.js
@@ -6,19 +6,18 @@ import css from '../images/CSS.png'
 import ruby from '../images/Ruby.jpeg'
 
 import express from '../images/node-express.png'
-import react from '../images/React.jpg'
+import react from '../images/React.jpeg'
 import bootstrap from '../images/Bootstrap.png'
 import sinatra from '../images/Sinatra.png'
 
 import rspec from '../images/Rspec.png'
 import jasmine from '../images/Jasmine.png'
-import jest from '../images/Rspec.png'
+import jest from '../images/Jest.png'
 
 import github from '../images/Github.png'
-import mongodb from '../images/Mongodb.png'
 import postgresql from '../images/PostgreSQL.png'
+import travisCI from '../images/travis-ci.jpg'
 import netlify from '../images/Netlify.png'
-import node from '../images/Node.png'
 
 function Skills() {
 
@@ -37,24 +36,22 @@ function Skills() {
       </div>
       <div className="fw-container">
         <h3> Frameworks </h3>
-        <img src={js} alt="JavaScript" />
-        <img src={html} alt="HTML" />
-        <img src={css} alt="CSS"/>
-        <img src={ruby} alt="Ruby"/>
+        <img src={express} alt="Express" />
+        <img src={react} alt="React" />
+        <img src={bootstrap} alt="Bootsrap"/>
       </div>
       <div className="testing-container">
         <h3> Testing </h3>
-        <img src={js} alt="JavaScript" />
-        <img src={html} alt="HTML" />
-        <img src={css} alt="CSS"/>
-        <img src={ruby} alt="Ruby"/>
+        <img src={rspec} alt="RSpec" />
+        <img src={jasmine} alt="Jasmine" />
+        <img src={jest} alt="Jest"/>
       </div>
       <div className="tools-container">
         <h3> Tools </h3>
-        <img src={js} alt="JavaScript" />
-        <img src={html} alt="HTML" />
-        <img src={css} alt="CSS"/>
-        <img src={ruby} alt="Ruby"/>
+        <img src={github} alt="Github" />
+        <img src={postgresql} alt="Mongodb" />
+        <img src={netlify} alt="Netlify"/>
+        <img src={travisCI} alt="TravisCI"/>
       </div>
     </div>
   );
