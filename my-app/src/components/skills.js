21d4b59629d00b55da766887b14f3ea68e541305
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-16 19:37:21 +0100

    Added extra container, restyled page

--- my-app/src/components/skills.js
+++ my-app/src/components/skills.js
@@ -1,8 +1,24 @@
 import React from 'react';
+// importing all images needed
 import js from '../images/JavaScript.png'
 import html from '../images/HTML.png'
 import css from '../images/CSS.png'
 import ruby from '../images/Ruby.jpeg'
+
+import express from '../images/node-express.png'
+import react from '../images/React.jpg'
+import bootstrap from '../images/Bootstrap.png'
+import sinatra from '../images/Sinatra.png'
+
+import rspec from '../images/Rspec.png'
+import jasmine from '../images/Jasmine.png'
+import jest from '../images/Rspec.png'
+
+import github from '../images/Github.png'
+import mongodb from '../images/Mongodb.png'
+import postgresql from '../images/PostgreSQL.png'
+import netlify from '../images/Netlify.png'
+import node from '../images/Node.png'
 
 function Skills() {
 
@@ -21,9 +37,15 @@ function Skills() {
       </div>
       <div className="fw-container">
         <h3> Frameworks </h3>
+
+      </div>
+      <div className="testing-container">
+        <h3> Testing </h3>
+
       </div>
       <div className="tools-container">
         <h3> Tools </h3>
+
       </div>
     </div>
   );
