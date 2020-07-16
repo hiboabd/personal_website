3e5f7f322563f0b3e2005b4ed27899cf7a357bfd
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-16 16:26:04 +0100

    Added skeleton html for skills page

--- my-app/src/components/skills.js
+++ my-app/src/components/skills.js
@@ -5,7 +5,19 @@ function Skills() {
 
   return (
     <div className="skills">
-      <h1> Skills </h1>
+      <div className="skills-header">
+        <h1> Skills </h1>
+        <h3> I have experience with the following technologies </h3>
+      </div>
+        <div className="lang-container">
+          
+        </div>
+        <div className="fw-container">
+
+        </div>
+        <div className="other-container">
+
+        </div>
     </div>
   );
 }
